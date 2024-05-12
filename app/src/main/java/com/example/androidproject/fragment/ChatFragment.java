d28069b77cd2251e44fdc2a21cb6d637701c5bf5
NTLinh <nguyenthilinh1942002@gmail.com>
2024-05-12 20:56:18 +0700

    Update ChatFragment.java

--- app/src/main/java/com/example/androidproject/fragment/ChatFragment.java
+++ app/src/main/java/com/example/androidproject/fragment/ChatFragment.java
@@ -117,6 +117,83 @@ public class ChatFragment extends Fragment {
 
         mDatabase = FirebaseDatabase.getInstance().getReference("Users");
         mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+    //         @Override
+    //         public void onDataChange(@NonNull DataSnapshot snapshot) {
+    //             listUserChat.clear();
+    //             for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+    //                 String userId = dataSnapshot.child("userId").getValue(String.class);
+    //                 String userName = dataSnapshot.child("userName").getValue(String.class);
+    //                 String profilePicture = dataSnapshot.child("profilePicture").getValue(String.class);
+    //                 String status = dataSnapshot.child("Status").getValue(String.class);
+
+    //                 // Đã có id người bạn đã chat
+    //                 // Check node id chat cuối cùng là mình với bạn mình để gán vào Object Chat
+    //                 getUserChattedToChats(userId, userName, profilePicture,status);
+    //             }
+    //             // sắp xếp lại thời gian mới nhất
+    //         }
+    //         @Override
+    //         public void onCancelled(@NonNull DatabaseError error) {
+
+    //         }
+    //     });
+    // }
+    // private void getUserChattedToChats(final String userId, final String userName, final String profilePicture,final String status) {
+    //     if (mUser == null) {
+    //         return;
+    //     }
+    //     mDatabase = FirebaseDatabase.getInstance().getReference("Chats");
+    //     mDatabase.addValueEventListener(new ValueEventListener() {
+    //         @Override
+    //         public void onDataChange(@NonNull DataSnapshot snapshot) {
+    //             listChat.clear();
+    //             if(snapshot.exists()){
+    //                     for (DataSnapshot dataSnapshot1 : snapshot.getChildren()) {
+    //                         Chat chat = dataSnapshot1.getValue(Chat.class);
+
+    //                         if (mUser != null && mUser.getUid() != null && chat != null && chat.getReceiverID() != null && chat.getSenderID() != null && userId != null) {
+    //                             if (mUser.getUid().equals(chat.getReceiverID()) && userId.equals(chat.getSenderID()) ||
+    //                                     userId.equals(chat.getReceiverID()) && mUser.getUid().equals(chat.getSenderID())) {
+    //                                 listChat.add(chat);
+    //                             }
+    //                         }
+    //                     }
+
+    //                 if (!listChat.isEmpty()) {
+    //                     int lastIndex = listChat.size() - 1;
+    //                     lastChat = listChat.get(lastIndex);
+    //                 }
+
+    //                 User user = new User(userId, userName, profilePicture, lastChat, status);
+
+
+    //                 for (String id : listIDFriendChat) {
+    //                     if (id != null && user.getUserId() != null && user.getUserId().equals(id)) {
+    //                         if (listUserChat.size() != 0) {
+    //                             boolean userExists = false;
+    //                             for (User user1 : listUserChat) {
+    //                                 if (user1.getUserId() != null && user.getUserId().equals(user1.getUserId())) {
+    //                                     userExists = true;
+    //                                     break;
+    //                                 }
+    //                             }
+    //                             if (!userExists) {
+    //                                 listUserChat.add(user);
+    //                             }
+    //                         } else {
+    //                             listUserChat.add(user);
+    //                         }
+    //                     }
+    //                 }
+
+
+    //                 itemUserAdapter = new ItemUserAdapter(listUserChat, getContext());
+    //                 rcvChat.setAdapter(itemUserAdapter);
+
+
+    //             }
+
+    //         }
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 listUserChat.clear();
@@ -194,6 +271,7 @@ public class ChatFragment extends Fragment {
                 }
 
             }
+            
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
